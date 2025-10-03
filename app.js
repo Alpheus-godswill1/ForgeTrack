fe32881f14251dac7e65c4b01b0a87d4b1d4c7bd
Godwins Godwin <godwinsudoinyang@gmail.com>
2025-10-03 17:20:08 +0100

    ft:
    Changed the command add to Add

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ ensureStorage();
 const command = process.argv[2];
 const task = process.argv[3];
 
-if (command === "add") {
+if (command === "Add") {
   addTask(task);
 } else if (command === "list") {
   listTasks(task);
